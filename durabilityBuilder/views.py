432c5b69041d576230298ba57b16073acbb61048
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2024-01-31 15:32:54 -0800

    added empty intro program pages

--- durabilityBuilder/views.py
+++ durabilityBuilder/views.py
@@ -31,6 +31,11 @@ def durabilitybuilder_index(request):
     return render(request, 'durabilityBuilder/index.html', {"form": form})
 
 
+def under_construction(request):
+
+    return render(request, 'durabilityBuilder/under_construction.html')
+
+
 def send_confirmation(request):
 
     return render(request, 'durabilityBuilder/send_confirmation.html')
@@ -48,7 +53,7 @@ def get_started(request):
             request.session["program_choice"] = program_choice
 
             if account == "a1":
-                return redirect("durabilityBuilder:send_confirm")
+                return redirect("durabilityBuilder:under_construction")
             else:
                 return redirect("durabilityBuilder:dashboard")
 
@@ -65,3 +70,75 @@ def user_is_staff(user):
 @login_required
 def dashboard(request):
     return render(request, 'durabilityBuilder/dashboard.html')
+
+
+@login_required
+def intro_day1(request):
+    return render(request, 'durabilityBuilder/intro_day1.html')
+
+
+@login_required
+def intro_day2(request):
+    return render(request, 'durabilityBuilder/intro_day2.html')
+
+
+@login_required
+def intro_day3(request):
+    return render(request, 'durabilityBuilder/intro_day3.html')
+
+
+@login_required
+def intro_day4(request):
+    return render(request, 'durabilityBuilder/intro_day4.html')
+
+
+@login_required
+def intro_day5(request):
+    return render(request, 'durabilityBuilder/intro_day5.html')
+
+
+@login_required
+def intro_day6(request):
+    return render(request, 'durabilityBuilder/intro_day6.html')
+
+
+@login_required
+def intro_day7(request):
+    return render(request, 'durabilityBuilder/intro_day7.html')
+
+
+@login_required
+def intro_day8(request):
+    return render(request, 'durabilityBuilder/intro_day8.html')
+
+
+@login_required
+def intro_day9(request):
+    return render(request, 'durabilityBuilder/intro_day9.html')
+
+
+@login_required
+def intro_day10(request):
+    return render(request, 'durabilityBuilder/intro_day10.html')
+
+
+@login_required
+def intro_day11(request):
+    return render(request, 'durabilityBuilder/intro_day11.html')
+
+
+@login_required
+def intro_day12(request):
+    return render(request, 'durabilityBuilder/intro_day12.html')
+
+
+@login_required
+def intro_day13(request):
+    return render(request, 'durabilityBuilder/intro_day13.html')
+
+
+@login_required
+def intro_day14(request):
+    return render(request, 'durabilityBuilder/intro_day14.html')
+
+
